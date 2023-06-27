6f5de0b9ba2858eaf8f1870f70581df4bd43b04e
Amirparsa Naghibi <amirparsanaghibi@gmail.com>
2023-06-27 20:27:52 +0330

    fixed bug

--- src/services/DummyData.js
+++ src/services/DummyData.js
@@ -1,17 +1,20 @@
+import section1 from '../assets/images/section1.webp';
+import section2 from '../assets/images/section2.webp';
+import section3 from '../assets/images/section3.webp';
 
 export const ServiceData = [
     {
-        image: './src/assets/images/section1.webp',
+        image: section1,
         title: 'Easy To Order',
         description: 'You only need a few steps in ordering food.'
     },
     {
-        image: './src/assets/images/section2.webp',
+        image: section2,
         title: 'Fastest Delivery ',
         description: 'Delivery that is always ontime even faster. '
     },
     {
-        image: './src/assets/images/section3.webp',
+        image: section3,
         title: 'Best Quality ',
         description: 'Not only fast for us quality is also number one. '
     },
