77a30fe38db9840836e028a735475ae904129d97
Amirparsa Naghibi <amirparsanaghibi@gmail.com>
2023-06-27 20:14:54 +0330

    fix ServiceData image bug

--- src/services/DummyData.js
+++ src/services/DummyData.js
@@ -1,17 +1,17 @@
 
 export const ServiceData = [
     {
-        image: '/src/assets/images/section1.webp',
+        image: './src/assets/images/section1.webp',
         title: 'Easy To Order',
         description: 'You only need a few steps in ordering food.'
     },
     {
-        image: '/src/assets/images/section2.webp',
+        image: './src/assets/images/section2.webp',
         title: 'Fastest Delivery ',
         description: 'Delivery that is always ontime even faster. '
     },
     {
-        image: '/src/assets/images/section3.webp',
+        image: './src/assets/images/section3.webp',
         title: 'Best Quality ',
         description: 'Not only fast for us quality is also number one. '
     },
