9c80adae021be0a1f531407c822bd787d06b460a
Amirparsa Naghibi <amirparsanaghibi@gmail.com>
2023-06-28 18:24:49 +0330

    Convert project to pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,52 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets:['maskable.png', 'favicon.ico' , 'touch.png'],
+      manifest: {
+        name: "Pana Food",
+        short_name: "Pana Food",
+        description: 'Selling food',
+        theme_color: "#E95051",
+        background_color: "#ffffff",
+        start_url: "/",
+        display: "standalone",
+        icons: [
+          {
+            src: "/touch.png",
+            size: "800x800",
+            type: "image/png"
+          },
+          {
+            src: "/192x192.png",
+            size: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/apple-touch-icon.png",
+            size: "512x512",
+            type: "image/png"
+          },
+          {
+            src: "/maskable.png",
+            size: "512x512",
+            type: "image/png",
+            purpose: 'any'
+          },
+          {
+            src: "/maskable.png",
+            size: "512x512",
+            type: "image/png",
+            purpose: 'maskable'
+          }
+        ],
+      },
+      
+    }),
+  ],
 })
