17219ff2858d3c09000d0a588661d114f1e038b3
Amirparsa Naghibi <amirparsanaghibi@gmail.com>
2023-06-30 21:24:01 +0330

    added Login from in mobile

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -14,6 +14,7 @@ import bg from '../assets/images/fast-food-dish-blue.webp';
 // component
 import Navbar from '../components/Navbar';
 import MobileHeader from '../components/MobileHeader';
+import { Link } from 'react-router-dom';
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
@@ -22,6 +23,7 @@ function classNames(...classes) {
 const User = () => {
 
     const [user, setUser] = useState({})
+    const [isWelcomeShow, setIsWelcomeShow] = useState(false)
 
     const [currentTap, setCurrentTap] = useState('Login');
 
@@ -33,22 +35,40 @@ const User = () => {
         }
     }
 
-    const submitHandler = (errors) => {
-    console.log(Object.keys(errors).length);
+    const submitHandlerSignUp = (errors) => {
     if (!Object.keys(errors).length) {
         toast.success('Registration successful')
     } else {
         toast.error("Registration was not successful")
     }
-
     }
+
+    const submitHandlerLogin = (errors) => {
+
+    if (Object.keys(user).length) {
+        if (!Object.keys(errors).length) {
+            toast.success('Login successful')
+            setIsWelcomeShow(true);
+        } else {
+            toast.error("Login was not successful")
+            setIsWelcomeShow(false)
+        }
+    } else {
+        toast('Register first!', {
+            icon: '❗',
+        });
+        setIsWelcomeShow(false)
+    }
+    
+    }
+    
 
     return (
         <div className='md:bg-primaryRed md:h-screen bg-none  bg-cover md:bg-contain bg-no-repeat bg-right bg-login-form'>
 
             <Toaster />
 
-            <div>
+            <div className='md:'>
                 <MobileHeader title={currentTap} />
             </div>
 
@@ -93,11 +113,86 @@ const User = () => {
                         
                             <Tab.Panel className=''>
                                 <div>
-                                    login
+                                    <Formik 
+                                    initialValues={{name: ''  , email: '', password: '' ,confirmPassword: '', checkbox:false}} 
+                                    onSubmit={(values) => {console.log(values)}}
+                                    validate={(values) => {
+                                        const errors = {};
+                                        // console.log(errors)
+                                        console.log(user);
+                                        
+                                        if (values.email !== user.email) {
+                                            errors.email = 'The email is not valid'
+                                        }
+
+                                        if (values.password !== user.password) {
+                                            errors.password = 'The password is not correct'
+                                        }
+
+                                        return errors;
+                                    }}
+                                    >
+                                        
+                                        {({errors, values, touched, }) => (
+                                            
+                                            <Form className='flex flex-col justify-center h-full relative bg-form gap-y-4'>
+                                                                                            
+                                                <div className='relative'>
+                                                    <Field
+                                                    type="email"
+                                                    name='email'
+                                                    id="email"
+                                                    />
+                                                    <label htmlFor="email" className={`labelForm transition-all duration-500 ${values.email && 'translate-y-[-120%] scale-90 transition-all duration-400'}`}>Email</label>
+                                                </div>
+                                                {errors.email && touched.email && <span className='bg-red-600 shadow-button shadow-red-500 text-gray-200 rounded-lg px-3 py-0.5 text-sm'>{errors.email}</span>}
+
+
+                                                <div className='relative'>
+                                                    <Field
+                                                    type="password"
+                                                    name='password'
+                                                    id="password"
+                                                    />
+                                                    <label htmlFor="password" className={`labelForm transition-all duration-500 ${values.password && 'translate-y-[-120%] scale-90 transition-all duration-400'}`}>Password</label>
+                                                </div>
+                                                {errors.password && touched.password && <span className='bg-red-600 shadow-button shadow-red-500 text-gray-200 rounded-lg px-3 py-0.5 text-sm'>{errors.password}</span>}
+                                                
+                                                <div className='pt-6'>
+                                                    <button 
+                                                    type='submit' 
+                                                    className={`bg-primaryRed  ${!Object.keys(errors).length && '!bg-green-600'}`} 
+                                                    onClick={() => submitHandlerLogin(errors)}>
+                                                        Login
+                                                    </button>
+                                                </div>
+                                            </Form>
+                                        )}
+                                        
+                                    </Formik>
+                                    <div className="shape1  bottom-[100px] right-0"></div>
+                                    <div className="shape2 top-[200px] left-[-100px]"></div>
                                 </div>
+
+                                {isWelcomeShow &&
+                                    <div className='bg-form p-8 mt-8 text-center'>
+                                        <h2 className='text-xl font-semibold text-slate-700 mb-8'>Welcome {user.name}</h2>
+
+                                        <div className=''>
+                                            <Link to={'/'}>
+                                            <button type='button' className='bg-primaryRed w-fit py-4 md:py-[22px] px-7 md:px-8 text-white rounded-full shadow-button md:text-2xl'>
+                                                Go To Home
+                                            </button>
+                                            </Link>
+                                        </div>
+                                    </div>
+                                }
+
                             </Tab.Panel>
 
-
+                            {/* ---------------------------------------------------------------------------------------------------------------------------------------- */}
+                            {/* ---------------------------------------------------------------------------------------------------------------------------------------- */}
+                            {/* ---------------------------------------------------------------------------------------------------------------------------------------- */}
 
                             <Tab.Panel className=''>
                                 <div>
@@ -106,7 +201,6 @@ const User = () => {
                                     onSubmit={(values) => {setUser(values)}}
                                     validate={(values) => {
                                         const errors = {};
-                                        console.log(errors)
                                         
                                         const trimmedUsername = values.name.trim();
                                         if (values.name.length < 4) {
@@ -201,7 +295,7 @@ const User = () => {
                                                     <button 
                                                     type='submit' 
                                                     className={`bg-primaryRed  ${!Object.keys(errors).length && '!bg-green-600'}`} 
-                                                    onClick={() => submitHandler(errors)}>
+                                                    onClick={() => submitHandlerSignUp(errors)}>
                                                         Register
                                                     </button>
                                                 </div>
