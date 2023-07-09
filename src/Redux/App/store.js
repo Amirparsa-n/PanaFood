26f0d1911408ea4041a70c97109f72adcdf2e557
Amirparsa Naghibi <amirparsanaghibi@gmail.com>
2023-07-09 12:18:03 +0330

    added favorite Food Slice
    by redux toolkit

--- src/Redux/App/store.js
+++ src/Redux/App/store.js
@@ -2,11 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 // reducer
 import cartReducer from "../Features/cartSlice";
+import favoriteFoodReducer from '../Features/favoriteFoodSlice';
 
 const store = configureStore({
     reducer: {
         cart: cartReducer,
-
+        favoriteFood: favoriteFoodReducer,
     }
 })
 
